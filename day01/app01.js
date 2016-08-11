36b0d0091cd04f71fd78cf4b572891f4fd70cf92
Charlie Arnold <charliearnold02@Charlie-Arnolds-MacBook.local>
2016-08-10 18:16:29 -0700

    created var a, b, c

---
+++ day01/app01.js
@@ -0,0 +1,8 @@
+//Charlie Arnold - 8/10/2016 - JavaScript 1
+var a = 12;
+var b = 0x1E;
+var c = "Answer equals:"
+var ab = a + b;
+var answer = c+ab;
+
+alert(answer);
---
+++ day01/script.js
@@ -0,0 +1,22 @@
+// alert('OMG BRO');
+var name  = 'charlie';
+var a = 5;
+var b = 6;
+var c = a + b;
+
+var e = true;
+var f = false;
+var g;
+
+var nothing = null;
+var something = undefined;
+
+var arr = ['charlie', 26, false];
+
+var car = {
+  make: 'Honda',
+  model: 'Element',
+  year: 2003
+};
+
+console.log( car.model );
